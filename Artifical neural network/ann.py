f31a6ed199504568bae566b5e2c374aec68a0fea
meet_cric <mp32445@gmai.com>
2018-09-10 02:36:57 +0530

    Data-preprocessing done

---
+++ Artifical neural network/ann.py
@@ -0,0 +1,10 @@
+#Artifical Neural Network
+
+#PART-1:Data Preprocessing
+#________________________________________________________
+
+#importing libararies
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
